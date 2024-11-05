32abde3e06bd9f6c89e2deed79deba52906e6042
from-import <dsearses6574@gmail.com>
2024-11-05 10:15:07 +0800

    Go

---
+++ LeetCode150/src/main/java/com/xxx/LeetCode61.java
@@ -0,0 +1,21 @@
+package com.xxx;
+
+import java.util.Arrays;
+
+public class LeetCode61 {
+    public int firstMissingPositive(int[] nums) {
+        int[] res = new int[100001];
+        // 将 `nums` 中的正整数标记在 `res` 中
+        for (int num : nums) {
+            if (num > 0 && num < 100001) {
+                res[num] = 1;  // 标记该位置，表示该正整数存在
+            }
+        }
+        for(int i = 1; i < res.length; i++){
+            if(res[i] == 0){
+                return i;
+            }
+        }
+        return 100001;
+    }
+}
