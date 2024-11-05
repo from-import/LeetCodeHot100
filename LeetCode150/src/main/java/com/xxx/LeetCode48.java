5267914a989797a143450e6d3e4e93945670c12a
from-import <dsearses6574@gmail.com>
2024-11-05 14:45:41 +0800

    GO

---
+++ LeetCode150/src/main/java/com/xxx/LeetCode48.java
@@ -0,0 +1,17 @@
+package com.xxx;
+
+public class LeetCode48 {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // 深拷贝 matrix -> tmp
+        int[][] tmp = new int[n][];
+        for (int i = 0; i < n; i++)
+            tmp[i] = matrix[i].clone();
+        // 根据元素旋转公式，遍历修改原矩阵 matrix 的各元素
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[j][n - 1 - i] = tmp[i][j];
+            }
+        }
+    }
+}
