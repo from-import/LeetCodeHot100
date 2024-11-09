7a9ebf985af797b09aeba304d025bd15ea415437
from-import <dsearses6574@gmail.com>
2024-11-09 14:54:48 +0800

    优化了两次二分找到左右边界的逻辑

--- LeetCode150/src/main/java/com/xxx/LeetCode34.java
+++ LeetCode150/src/main/java/com/xxx/LeetCode34.java
@@ -51,65 +51,44 @@ public class LeetCode34 {
      * @return
      */
     public int[] searchRange2(int[] nums, int target) {
+        // 思路 ： 两次二分，分别找到左右边界
+        int finalLeft = 0;
+        int finalRight = 0;
         if (nums.length == 0) {
             return new int[]{-1, -1};
         }
-
-        if (nums.length == 0) {
-            return new int[]{-1, -1};
-        }
+        // 第1次二分，找到最左边界
         int left = 0;
         int right = nums.length - 1;
-        int finalLeft = -1;
-        int finalRight = -1;
-
-        // 找到最左边界
-        while(left <= right){
+        while (right >= left) {
             int mid = left + (right - left) / 2;
-            // 1.找到了
-            if(nums[mid] == target){
+            if (nums[mid] >= target){
                 right = mid - 1;
             }
-            // 2. 比 mid 大
-            else if(nums[mid] > target){
-                right = mid - 1;
-            }
-            // 3. 比 mid 小
-            else{
+            else if (nums[mid] < target) {
                 left = mid + 1;
             }
         }
-        // 检查是否找到了 target 的左边界
-        if (left < nums.length && nums[left] == target) {
+        // 判断 left 是否在数组range 且 nums[left] == target
+        if (left <= nums.length - 1 && nums[left] == target){
             finalLeft = left;
         } else {
-            return new int[]{-1, -1}; // 如果没有找到 target，直接返回 [-1, -1]
+            return new int[]{-1, -1};
         }
 
-        // 重置 left and right
+        // 第2次二分，找到最右边界
         left = 0;
         right = nums.length - 1;
-
-        // 找到最右边界
-        while(left <= right){
+        while (right >= left) {
             int mid = left + (right - left) / 2;
-            // 1.找到了
-            if(nums[mid] == target){
+            if (nums[mid] <= target){
                 left = mid + 1;
             }
-            // 2. 比 mid 大
-            else if(nums[mid] > target){
+            else if (nums[mid] > target) {
                 right = mid - 1;
             }
-            // 3. 比 mid 小
-            else{
-                left = mid + 1;
-            }
         }
-        // 检查是否找到了 target 的右边界
-        if (right >= 0 && nums[right] == target) {
-            finalRight = right;
-        }
+        finalRight = right;
         return new int[]{finalLeft, finalRight};
     }
 }
