e205acbf3ad96b55bcc5766134b4a6bcd3732b80
from-import <dsearses6574@gmail.com>
2024-11-15 15:43:59 +0800

    我看不懂啊

--- LeetCode150/src/main/java/com/xxx/LeetCode763.java
+++ LeetCode150/src/main/java/com/xxx/LeetCode763.java
@@ -1,5 +1,8 @@
 package com.xxx;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeetCode763 {
     /**
      * 详细解释代码逻辑：
@@ -16,4 +19,31 @@ public class LeetCode763 {
      *         将 start 更新为 i + 1 = 9，开始寻找下一个片段。
      *         从 i = 9 继续重复以上步骤，找到下一个片段。
      */
+
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> partitions = new ArrayList<>();
+
+        // 记录每个字符最后出现的位置
+        int[] lastOccurrence = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            lastOccurrence[s.charAt(i) - 'a'] = i;
+        }
+
+        // 遍历字符串，确定分区
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            // 更新当前片段的结束位置
+            end = Math.max(end, lastOccurrence[s.charAt(i) - 'a']);
+
+            // 当到达当前片段的结束位置时
+            if (i == end) {
+                // 将当前片段的长度添加到结果
+                partitions.add(end - start + 1);
+                // 更新起始位置为下一个片段的开始位置
+                start = i + 1;
+            }
+        }
+
+        return partitions;
+    }
 }
