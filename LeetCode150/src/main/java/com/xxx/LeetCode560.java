6b37cb673fa871f51dd112e05fe288fffc9d5bb6
from-import <dsearses6574@gmail.com>
2024-12-10 13:55:08 +0800

    前缀和启动

--- LeetCode150/src/main/java/com/xxx/LeetCode560.java
+++ LeetCode150/src/main/java/com/xxx/LeetCode560.java
@@ -3,21 +3,35 @@ package com.xxx;
 import java.util.HashMap;
 
 public class LeetCode560 {
+    // 暴力方法，时间复杂度O(n^2)
     public int subarraySum(int[] nums, int k) {
-        HashMap<Integer, Integer> prefixSumCount = new HashMap<>();
-        prefixSumCount.put(0, 1); // 初始条件：前缀和为 0 出现 1 次
-        int currentSum = 0; // 当前前缀和
-        int count = 0; // 记录子数组个数
+        int target = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sumNow = 0;
+            for (int j = i; j < nums.length; j++) {
+                sumNow += nums[j];
+                if (sumNow == k) {
+                    target += 1;
+                }
+            }
+        }
+        return target;
+    }
+
+    public int subarraySum2(int[] nums, int k) {
+        int target = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        int count = 0;
+        int currentSum = 0;
         for (int num : nums) {
             currentSum += num;
-            if (null != prefixSumCount.get(currentSum - k)) {
-                count += prefixSumCount.get(currentSum - k);
+            if (null != map.get(currentSum - k)) {
+                count += map.get(currentSum - k);
             }
-
             // 更新哈希表中当前前缀和的出现次数
-            prefixSumCount.put(currentSum, prefixSumCount.getOrDefault(currentSum, 0) + 1);
-
+            map.put(currentSum, map.getOrDefault(currentSum, 0) + 1);
         }
-        return count ;
+        return count;
     }
 }
