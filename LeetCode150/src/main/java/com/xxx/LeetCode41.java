b6be51189cf38818a14a6078ee64ef1a7905fb44
from-import <dsearses6574@gmail.com>
2024-12-17 17:15:39 +0800

    On时间复杂度 + O1空间复杂度

---
+++ LeetCode150/src/main/java/com/xxx/LeetCode41.java
@@ -0,0 +1,27 @@
+package com.xxx;
+
+public class LeetCode41 {
+    public int firstMissingPositive(int[] nums) {
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            while (nums[i] > 0 && nums[i] <= length && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return length + 1;
+    }
+
+    // 交换数组中下标 i 和 j 的元素
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
