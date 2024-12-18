a0704d2ab263489ea30055e2b4caeaaaadaa68cf
from-import <dsearses6574@gmail.com>
2024-12-18 16:42:37 +0800

    链表找环出现的点，最优

---
+++ LeetCode150/src/main/java/com/xxx/LeetCode142.java
@@ -0,0 +1,23 @@
+package com.xxx;
+
+public class LeetCode142 {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                // isCircle == true
+                slow = head;
+                // 逮到你了
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
