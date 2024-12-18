a0f94899a579d132ddc8864be387152735aec560
from-import <dsearses6574@gmail.com>
2024-12-18 14:12:27 +0800

    finish

--- LeetCode150/src/main/java/com/xxx/LeetCode73.java
+++ LeetCode150/src/main/java/com/xxx/LeetCode73.java
@@ -2,63 +2,63 @@ package com.xxx;
 
 public class LeetCode73 {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        boolean firstRowZero = false;
-        boolean firstColZero = false;
-
-        // 1. 检查第一行是否需要置零
-        for (int j = 0; j < n; j++) {
-            if (matrix[0][j] == 0) {
-                firstRowZero = true;
-                break;
+        boolean firstRow = false;
+        boolean firstCol = false;
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (matrix[0][i] == 0) {
+                firstRow = true;
             }
         }
-
-        // 2. 检查第一列是否需要置零
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             if (matrix[i][0] == 0) {
-                firstColZero = true;
-                break;
+                firstCol = true;
             }
         }
 
-        // 3. 使用第一行和第一列作为标记数组
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
                 if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
                     matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
 
-        // 4. 根据标记置零
-        for (int i = 1; i < m; i++) {
+        for (int i = 1; i < matrix[0].length; i++) {
+            if (matrix[0][i] == 0) {
+                setZeroCol(matrix, i);
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
             if (matrix[i][0] == 0) {
-                for (int j = 1; j < n; j++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        for (int j = 1; j < n; j++) {
-            if (matrix[0][j] == 0) {
-                for (int i = 1; i < m; i++) {
-                    matrix[i][j] = 0;
-                }
+                setZeroRow(matrix, i);
             }
         }
 
-        // 5. 最后处理第一行和第一列
-        if (firstRowZero) {
-            for (int j = 0; j < n; j++) {
-                matrix[0][j] = 0;
-            }
+        // 如果第一行需要置零
+        if (firstRow) {
+            setZeroRow(matrix, 0);
         }
-        if (firstColZero) {
-            for (int i = 0; i < m; i++) {
-                matrix[i][0] = 0;
-            }
+
+        // 如果第一列需要置零
+        if (firstCol) {
+            setZeroCol(matrix, 0);
+        }
+
+
+    }
+
+    // 将指定行的所有列设置为 0
+    public void setZeroRow(int[][] matrix, int row) {
+        for (int i = 0; i < matrix[0].length; i++) { // 遍历列
+            matrix[row][i] = 0; // 修改当前行的每一列
+        }
+    }
+
+    // 将指定列的所有行设置为 0
+    public void setZeroCol(int[][] matrix, int col) {
+        for (int i = 0; i < matrix.length; i++) { // 遍历行
+            matrix[i][col] = 0; // 修改当前列的每一行
         }
     }
 
