ccec4d7b4babdcb640841f4dc042c98423c1ec80
from-import <dsearses6574@gmail.com>
2024-12-18 13:51:32 +0800

    On时间复杂度 + O1空间复杂度

--- LeetCode150/src/main/java/com/xxx/LeetCode41.java
+++ LeetCode150/src/main/java/com/xxx/LeetCode41.java
@@ -3,23 +3,28 @@ package com.xxx;
 public class LeetCode41 {
     public int firstMissingPositive(int[] nums) {
         int length = nums.length;
+
         for (int i = 0; i < length; i++) {
-            while (nums[i] > 0 && nums[i] <= length && nums[i] != nums[nums[i] - 1]) {
+            // 遍历数组中的每一个元素，i 是当前元素的索引
+            while (nums[i] > 0 && nums[i] < length && nums[i] != nums[nums[i] - 1]) {
+                // 1. nums[i] > 0：跳过非正数（负数或零），因为我们只关心正整数。
+                // 2. nums[i] < length：如果 nums[i] >= length，说明它超出范围，跳过处理。
+                // 3. nums[i] != nums[nums[i] - 1]：检查当前数字 nums[i] 是否已经放在正确位置。
+                //    - nums[i] 应该在索引 nums[i] - 1 的位置上。
+
+                // 如果以上条件都满足，就交换 nums[i] 和 nums[nums[i] - 1]，将当前数字放到它的目标位置。
                 swap(nums, i, nums[i] - 1);
             }
         }
-
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < length; i++) {
             if (nums[i] != i + 1) {
                 return i + 1;
             }
         }
-
         return length + 1;
     }
 
-    // 交换数组中下标 i 和 j 的元素
-    private void swap(int[] nums, int i, int j) {
+    public void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
