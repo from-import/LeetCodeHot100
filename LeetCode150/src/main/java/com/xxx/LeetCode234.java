251de9b7bf0d9573d169fc78aa7face3a35af662
from-import <dsearses6574@gmail.com>
2025-01-10 19:28:28 +0800

    暴力解 + 优化解

--- LeetCode150/src/main/java/com/xxx/LeetCode234.java
+++ LeetCode150/src/main/java/com/xxx/LeetCode234.java
@@ -1,8 +1,33 @@
 package com.xxx;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeetCode234 {
+    // O(n) 空间复杂度
+    public boolean isPalindrome1(ListNode head) {
+        // 用数组存储链表的值
+        List<Integer> values = new ArrayList<>();
+        ListNode current = head;
+        while (current != null) {
+            values.add(current.val);
+            current = current.next;
+        }
+
+        // 用双指针判断是否是回文
+        int left = 0;
+        int right = values.size() - 1;
+        while (left < right) {
+            if (!values.get(left).equals(values.get(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
     // O(n) 时间复杂度 + O(1) 空间复杂度
-    public boolean isPalindrome(ListNode head) {
+    public boolean isPalindrome2(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
         while (fast != null && fast.next != null) {
