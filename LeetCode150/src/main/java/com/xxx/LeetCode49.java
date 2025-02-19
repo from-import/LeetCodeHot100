bd955a8a595b6bf106b976ccb04d6fe35fceb09c
from-import <dsearses6574@gmail.com>
2025-02-19 22:07:08 +0800

    升级为按照chat -> String -> Arrays.sort(String) -> map.put的逻辑

--- LeetCode150/src/main/java/com/xxx/LeetCode49.java
+++ LeetCode150/src/main/java/com/xxx/LeetCode49.java
@@ -5,33 +5,29 @@ import java.util.*;
 class LeetCode49 {
 
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res = new ArrayList<>();
-        HashMap<Integer, List<String>> map = new HashMap<>();
-
+        HashMap<String, List<String>> map = new HashMap<>();
+        // 一次遍历，按照sort后的char[]作为key填入map
+        // 如果为null，new一个List<String>
+        // 如果不为null，.add
         for (String s : strs) {
-            int hashKey = wordToHash(s);
-
-            // 如果不存在键，初始化对应的列表
-            if (!map.containsKey(hashKey)) {
-                map.put(hashKey, new ArrayList<>());
+            char[] arrayString = s.toCharArray();
+            Arrays.sort(arrayString);
+            String mid = new String(arrayString);
+            if (map.get(mid) == null) {
+                List<String> res = new ArrayList<>();
+                res.add(s);
+                map.put(mid, res);
             }
-
-            map.get(hashKey).add(s);
+            else {
+                map.get(mid).add(s);
+            }
         }
 
-        // 遍历 HashMap，将每个分组添加到结果列表
-        for (Map.Entry<Integer, List<String>> entry : map.entrySet()) {
-            res.add(entry.getValue()); // 只需要分组的值部分
+        List<List<String>> result = new ArrayList<>();
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            result.add(entry.getValue());
         }
-
-        return res; // 返回分组结果
-    }
-
-    public int wordToHash(String s) {
-        char[] res = s.toCharArray();
-        Arrays.sort(res);
-        String finalString = new String(res);
-        return finalString.hashCode();
+        return result;
     }
 
 
