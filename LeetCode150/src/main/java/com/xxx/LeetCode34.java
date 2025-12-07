d1d01bcc81c7f1001f2e8cee9eb4f644a5f8e747
from-import <dsearses6574@gmail.com>
2025-12-07 14:26:31 +0800

    34. Find First and Last Position of Element in Sorted Array

--- LeetCode150/src/main/java/com/xxx/LeetCode34.java
+++ LeetCode150/src/main/java/com/xxx/LeetCode34.java
@@ -2,93 +2,46 @@ package com.xxx;
 
 public class LeetCode34 {
     public int[] searchRange(int[] nums, int target) {
-        if (nums.length == 0) {
-            return new int[]{-1, -1};
+        int arg1 = leftBinarySearch(nums, target);
+        int arg2 = rightBinarySearch(nums, target) - 1;
+
+        if (arg1 == nums.length || nums[arg1] != target) {
+            arg1 = -1;
+        }
+        if (arg2 == nums.length || arg2 == -1 || nums[arg2] != target) {
+            arg2 = -1;
         }
 
-        int left = 0, right = nums.length - 1;
-        int findMe = -1; // 修改 findMe 的初始值
-
-        // 二分查找找到 target 的任意一个位置
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                findMe = mid;
-                break;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        // 如果没有找到 target，返回 [-1, -1]
-        if (findMe == -1) {
-            return new int[]{-1, -1};
-        }
-
-        // 扩展 findMe 找到左右边界
-        left = findMe;
-        right = findMe;
-
-        // 向左扩展边界，找到起始位置
-        while (left > 0 && nums[left - 1] == target) {
-            left--;
-        }
-
-        // 向右扩展边界，找到结束位置
-        while (right < nums.length - 1 && nums[right + 1] == target) {
-            right++;
-        }
-
-        return new int[]{left, right};
+        return new int[]{arg1, arg2};
     }
+    public int leftBinarySearch(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int index = (start + end) / 2;
+            int value = nums[index];
 
-    /**
-     * 两次二分找左右
-     * @param nums
-     * @param target
-     * @return
-     */
-    public int[] searchRange2(int[] nums, int target) {
-        // 思路 ： 两次二分，分别找到左右边界
-        int finalLeft = 0;
-        int finalRight = 0;
-        if (nums.length == 0) {
-            return new int[]{-1, -1};
-        }
-        // 第1次二分，找到最左边界
-        int left = 0;
-        int right = nums.length - 1;
-        while (right >= left) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] >= target){
-                right = mid - 1;
-            }
-            else if (nums[mid] < target) {
-                left = mid + 1;
+            if (value < target) {
+                start = index + 1;
+            } else {
+                end = index - 1;
             }
         }
-        // 判断 left 是否在数组range 且 nums[left] == target
-        if (left <= nums.length - 1 && nums[left] == target){
-            finalLeft = left;
-        } else {
-            return new int[]{-1, -1};
-        }
+        return start;
+    }
+    public int rightBinarySearch(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int index = (start + end) / 2;
+            int value = nums[index];
 
-        // 第2次二分，找到最右边界
-        left = 0;
-        right = nums.length - 1;
-        while (right >= left) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] <= target){
-                left = mid + 1;
-            }
-            else if (nums[mid] > target) {
-                right = mid - 1;
+            if (value > target) {
+                end = index - 1;
+            } else {
+                start = index + 1;
             }
         }
-        finalRight = right;
-        return new int[]{finalLeft, finalRight};
+        return start;
     }
 }
