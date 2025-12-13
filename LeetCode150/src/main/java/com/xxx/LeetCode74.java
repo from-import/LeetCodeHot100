8b9eb9ca835bd2323a21963e4557445d68d367f4
from-import <dsearses6574@gmail.com>
2025-12-13 22:15:07 +0800

    74. Search a 2D Matrix

--- LeetCode150/src/main/java/com/xxx/LeetCode74.java
+++ LeetCode150/src/main/java/com/xxx/LeetCode74.java
@@ -2,45 +2,57 @@ package com.xxx;
 
 public class LeetCode74 {
     public boolean searchMatrix(int[][] matrix, int target) {
-        // 思路：先二分找到行，然后再二分找到列，两次二分解决
-        int[] col = halfSearch2(matrix, target);
-        return halfSearch(col, target);
+        if (matrix == null || target < matrix[0][0]) {
+            return false;
+        }
 
+        // 找到第一列的第一个比target小的数的位置
+        int[] tempMartix = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            tempMartix[i] = matrix[i][0];
+        }
+        int row = upperBinarySearch(tempMartix, target) - 1;
+        if (row == -1) {
+            return false;
+        }
+
+        int col = lowerBinarySearch(matrix[row], target);
+        return (col < matrix[0].length && matrix[row][col] == target);
     }
 
-    public boolean halfSearch(int[] nums, int target){
+    // 返回第一个 > t 的位置
+    private int upperBinarySearch(int[] nums, int target) {
+        int length = nums.length;
         int left = 0;
-        int right = nums.length - 1;
-        while(right >= left) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target){
-                return true;
+        int right = length - 1;
+        while (left <= right) {
+            int index = (left + right) / 2;
+            int value = nums[index];
+            if (value <= target) {
+                left = index + 1;
+            } else {
+                right = index - 1;
             }
-            else if (nums[mid] > target) {
-                right = mid - 1;
-            }
-            else if (nums[mid] < target) {
-                left = mid + 1;
+
+        }
+        return left;
+    }
+
+    // 返回第一个 ≥ t 的位置
+    private int lowerBinarySearch(int[] nums, int target) {
+        int length = nums.length;
+        int left = 0;
+        int right = length - 1;
+        while (left <= right) {
+            int index = (left + right) / 2;
+            int value = nums[index];
+            if (value >= target) {
+                right = index - 1;
+            } else {
+                left = index + 1;
             }
         }
-        return false;
+        return left;
     }
 
-    public int[] halfSearch2(int[][] nums, int target){
-        int left = 0;
-        int right = nums.length - 1;
-        while(right >= left) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid][0] == target){
-                return nums[mid];
-            }
-            else if (nums[mid][0] > target) {
-                right = mid - 1;
-            }
-            else if (nums[mid][0] < target) {
-                left = mid + 1;
-            }
-        }
-        return left >= 1 ? nums[left - 1] : nums[left];
-    }
 }
