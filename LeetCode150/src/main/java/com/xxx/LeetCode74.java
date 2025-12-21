244e750d5c08d202b560fdae2de764966f4f352b
from-import <dsearses6574@gmail.com>
2025-12-21 14:43:15 +0800

    74. Search a 2D Matrix

--- LeetCode150/src/main/java/com/xxx/LeetCode74.java
+++ LeetCode150/src/main/java/com/xxx/LeetCode74.java
@@ -2,54 +2,45 @@ package com.xxx;
 
 public class LeetCode74 {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || target < matrix[0][0]) {
+        if (matrix.length == 0) {
             return false;
         }
-
-        // 找到第一列的第一个比target小的数的位置
-        int[] tempMartix = new int[matrix.length];
+        int[] firstCol = new int[matrix.length];
         for (int i = 0; i < matrix.length; i++) {
-            tempMartix[i] = matrix[i][0];
+            firstCol[i] = matrix[i][0];
         }
-        int row = upperBinarySearch(tempMartix, target) - 1;
-        if (row == -1) {
+        int firstColLowerThanTarget = upperBinarySearch(firstCol, target) - 1;
+        if (firstColLowerThanTarget < 0) {
             return false;
         }
-
-        int col = lowerBinarySearch(matrix[row], target);
-        return (col < matrix[0].length && matrix[row][col] == target);
-    }
-
-    // 返回第一个 > t 的位置
-    private int upperBinarySearch(int[] nums, int target) {
-        int length = nums.length;
         int left = 0;
-        int right = length - 1;
-        while (left <= right) {
-            int index = (left + right) / 2;
-            int value = nums[index];
-            if (value <= target) {
-                left = index + 1;
-            } else {
-                right = index - 1;
-            }
+        int right = matrix[firstColLowerThanTarget].length - 1;
 
-        }
-        return left;
-    }
-
-    // 返回第一个 ≥ t 的位置
-    private int lowerBinarySearch(int[] nums, int target) {
-        int length = nums.length;
-        int left = 0;
-        int right = length - 1;
         while (left <= right) {
-            int index = (left + right) / 2;
-            int value = nums[index];
+            int middle = left + (right - left) / 2;
+            int value = matrix[firstColLowerThanTarget][middle];
             if (value >= target) {
-                right = index - 1;
+                right = middle - 1;
             } else {
-                left = index + 1;
+                left = middle + 1;
+            }
+        }
+        if (left >= matrix[firstColLowerThanTarget].length) {
+            return false;
+        }
+        return matrix[firstColLowerThanTarget][left] == target;
+
+    }
+    private int upperBinarySearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            int middleValue = nums[middle];
+            if (middleValue > target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
             }
         }
         return left;
