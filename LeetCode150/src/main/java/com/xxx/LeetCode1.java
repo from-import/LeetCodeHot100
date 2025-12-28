c8f11b1e5732456d81285fd25c93fb30b8249b9f
from-import <dsearses6574@gmail.com>
2025-12-28 16:06:28 +0800

    1. Two Sum

--- LeetCode150/src/main/java/com/xxx/LeetCode1.java
+++ LeetCode150/src/main/java/com/xxx/LeetCode1.java
@@ -1,17 +1,21 @@
 package com.xxx;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class LeetCode1 {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int number = nums[i];
-            if (map.get(number) != null) {
-                return new int[]{i , map.get(number)};
-            }
-            map.put(target - number, i);
+        if (nums == null || nums.length < 2) {
+            return new int[]{-1, -1};
         }
-        return new int[]{0, 0};
+        Map<Integer, Integer> sumMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (sumMap.get(nums[i]) != null) {
+                return new int[]{i, sumMap.get(nums[i])};
+            }
+            sumMap.put(target - nums[i], i);
+        }
+        return new int[]{-1, -1};
+
     }
 }
