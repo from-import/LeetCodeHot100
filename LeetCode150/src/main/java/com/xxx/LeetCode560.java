238dbcbdbf737824d820a046ac29149bc0ebbe8a
from-import <dsearses6574@gmail.com>
2026-01-02 14:48:41 +0800

    560. Subarray Sum Equals K

--- LeetCode150/src/main/java/com/xxx/LeetCode560.java
+++ LeetCode150/src/main/java/com/xxx/LeetCode560.java
@@ -1,10 +1,11 @@
 package com.xxx;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class LeetCode560 {
     // 暴力方法，时间复杂度O(n^2)
-    public int subarraySum(int[] nums, int k) {
+    public int subarraySum1(int[] nums, int k) {
         int target = 0;
         for (int i = 0; i < nums.length; i++) {
             int sumNow = 0;
@@ -33,5 +34,24 @@ public class LeetCode560 {
             map.put(currentSum, map.getOrDefault(currentSum, 0) + 1);
         }
         return count;
+    }
+
+    public int subarraySum(int[] nums, int k) {
+        int[] prefix = new int[nums.length];
+        prefix[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+
+        Map<Integer, Integer> count = new HashMap<>();
+        count.put(0, 1);
+        int res = 0;
+
+        for (int p : prefix) {
+            res += count.getOrDefault(p - k, 0);
+            count.put(p, count.getOrDefault(p, 0) + 1);
+        }
+        return res;
+
     }
 }
