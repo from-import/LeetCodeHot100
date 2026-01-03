bd510df87d017ea7e9792f080305d7699ac53a76
from-import <dsearses6574@gmail.com>
2026-01-03 16:48:49 +0800

    45. Jump Game II

--- LeetCode150/src/main/java/com/xxx/LeetCode45.java
+++ LeetCode150/src/main/java/com/xxx/LeetCode45.java
@@ -21,4 +21,54 @@ public class LeetCode45 {
 
         return count;
     }
+
+    /**
+     * O(n^2)
+     */
+    public int jump2(int[] nums) {
+        if (nums.length == 1) {
+            return 0;
+        }
+        int maxJump = nums[0];
+        int index = 0;
+        int count = 0;
+        while (index < nums.length) {
+            count++;
+            int value = nums[index];
+            int maxReached = nums[index] + index;
+            if (maxReached >= nums.length - 1) {
+                return count;
+            }
+            int farIndex = index;
+
+            for (int j = index+1; j <= Math.min(nums.length - 1, index + value); j++) {
+                // forEach the value range
+                if (nums[j] + j >= maxReached) {
+                    maxReached = nums[j] + j;
+                    farIndex = j;
+                    if (maxReached >= nums.length - 1) {
+                        return count + 1;
+                    }
+                }
+            }
+            index = farIndex;
+
+        }
+        return -1;
+    }
+
+    /**
+     * O(n)
+     */
+    public int jump3(int[] nums) {
+        int steps = 0, end = 0, farthest = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if (i == end) {
+                steps++;
+                end = farthest;
+            }
+        }
+        return steps;
+    }
 }
