9accd26fc980eb38439483135b836d48647c32ef
from-import <dsearses6574@gmail.com>
2026-03-28 23:27:20 +0800

    101. Symmetric Tree

--- LeetCode150/src/main/java/com/xxx/LeetCode101.java
+++ LeetCode150/src/main/java/com/xxx/LeetCode101.java
@@ -15,4 +15,26 @@ public class LeetCode101 {
 
         return(left.val == right.val) && (function(left.left,right.right)) && (function(left.right, right.left));
     }
+
+    public boolean isSymmetric2(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return check(root.left, root.right);
+    }
+
+    private boolean check(TreeNode left, TreeNode right) {
+        if (left == null && right != null) {
+            return false;
+        }
+        if (left != null && right == null) {
+            return false;
+        }
+        if (left == null && right == null) {
+            return true;
+        }
+        return (left.val == right.val
+                && check(left.left, right.right)
+                && check(left.right, right.left));
+    }
 }
